60455ada1eef8de7c25820f88062ccc668478725
Andrzej Żernaczuk <andrzej.zernaczuk@gmail.com>
2023-07-09 18:21:31 +0200

    added salary cap transform, changed seasons to years

--- clean_data.py
+++ clean_data.py
@@ -11,8 +11,16 @@ def clean_stats(year: int, game_type: str, stats_category: str, drop_cols: list)
 
     if stats_category in ["players_stats", "players_advanced_stats"]:
         df_dropped.rename(columns = {'Player-additional':'ID'}, inplace = True)
+        # find duplicates
+        duplicates_df = df_dropped[df_dropped.duplicated(['ID'], keep=False)]
+        # store the last team
+        dropped_duplicates_df = duplicates_df.drop_duplicates(subset='ID', keep="last")
+        # drop duplicates
         filtered_df = df_dropped.drop_duplicates(subset='ID', keep="first")
-        filtered_df.reset_index(drop=True, inplace=True)
+        filtered_df = filtered_df.set_index("ID")
+        # swap TOT for last team ID
+        for index, row in dropped_duplicates_df.iterrows():
+            filtered_df.loc[[f"{row['ID']}"], ['Tm']] = dropped_duplicates_df.query(f"ID == '{row['ID']}'")["Tm"].iloc[0]
     if stats_category == "teams_stats":
         filtered_df = df_dropped[df_dropped.Team != "League Average"]
         filtered_df['Team'] = filtered_df['Team'].str.replace('*', '')
@@ -43,11 +51,12 @@ def consolidate_personal_awards():
     sixth_man_awards = pd.read_csv("./data/league_awards/sixth_man_awards.csv")
 
     # create dataframe
-    df_columns = ["season", "defensive", "most_improved", "most_valuable", "rookie", "sixth_man"]
+    df_columns = ["year", "defensive", "most_improved", "most_valuable", "rookie", "sixth_man"]
     awards_conso = pd.DataFrame(columns=df_columns)
-    awards_conso["season"] = def_awards["Season"]
+    awards_conso["year"] = def_awards["Season"]
+    awards_conso['year'] = awards_conso['year'].apply(lambda x: int("20" + str(x)[-2:]))
 
-    # add awards to dataframe by season
+    # add awards to dataframe by year
     awards_conso["defensive"] = def_awards["ID"]
     awards_conso["most_improved"] = improv_awards["ID"]
     awards_conso["most_valuable"] = mvp_awards["ID"]
@@ -55,3 +64,16 @@ def consolidate_personal_awards():
     awards_conso["sixth_man"] = sixth_man_awards["ID"]
 
     return awards_conso
+
+def consolidate_salary_cap():
+    """Consolidate salary cap"""
+
+    # read csv
+    salary_cap_read = pd.read_csv("./data/teams_stats/salary_cap_history.csv")
+
+    salary_cap = salary_cap_read.drop("2022 Dollars", axis=1)
+    salary_cap.rename(columns = {'Year': 'year', 'Salary Cap': 'salary_cap'}, inplace = True)
+    salary_cap['year'] = salary_cap['year'].apply(lambda x: int("20" + str(x)[-2:]))
+    salary_cap['salary_cap'] = salary_cap['salary_cap'].apply(lambda x: int(str(x)[1:].replace(",", "")))
+
+    return salary_cap
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from get_data import get_players_stats, get_team_stats, get_players_advanced_stats
-from clean_data import clean_stats, unique_players, consolidate_personal_awards
+from clean_data import clean_stats, unique_players, consolidate_personal_awards, consolidate_salary_cap
 import csv
 import pickle
 
-years = list(range(2001, 2023))
+years = list(range(2001, 2024))
 game_types = ["playoffs", "leagues"]
 # stats that aren't need for model
 players_columns_to_drop = ['Rk', 'Pos', 'FG', 'FGA', 'FG%', '3P',
@@ -23,10 +23,11 @@ players_stats = {}
 players_advanced_stats = {}
 teams_stats = {}
 
-# get_players_stats(years, game_types)
-# get_team_stats(years, game_types)
-# get_players_advanced_stats(years, game_types)
+get_players_stats(years, game_types)
+get_team_stats(years, game_types)
+get_players_advanced_stats(years, game_types)
 personal_awards = consolidate_personal_awards()
+salary_cap = consolidate_salary_cap()
 for year in years:
     distinct_players.extend(unique_players(year))
     players_stats[year] = {}
@@ -41,16 +42,13 @@ distinct_players = set(distinct_players)
 
 # store processed data
 personal_awards.to_csv('./data/transformed_data/personal_awards.csv', index=False, encoding='utf-8')
-
+salary_cap.to_csv('./data/transformed_data/salary_cap.csv', index=False, encoding='utf-8')
 with open('./data/transformed_data/distinct_players.csv', 'w') as dist_play:
     write = csv.writer(dist_play)
     write.writerow(distinct_players)
-
 with open('./data/transformed_data/players_stats.pickle', 'wb') as play_stats:
     pickle.dump(players_stats, play_stats)
-
 with open('./data/transformed_data/players_advanced_stats.pickle', 'wb') as play_adv_stats:
     pickle.dump(players_advanced_stats, play_adv_stats)
-
 with open('./data/transformed_data/teams_stats.pickle', 'wb') as team_stats:
     pickle.dump(teams_stats, team_stats)
